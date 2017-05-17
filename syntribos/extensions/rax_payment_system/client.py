e456820df690670052b6af356b2e5529145e8161
Michael Dong <michael.dong@rackspace.com>
2017-05-17 00:16:33 -0500

    extension client

--- syntribos/extensions/rax_payment_system/client.py
+++ syntribos/extensions/rax_payment_system/client.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 import logging
 import urlparse
-import xml.etree.ElementTree as ET
+import xmltodict
 
 from oslo_config import cfg
 
@@ -45,12 +45,12 @@ def list_paymentMethods(ran=CONF.rax_payment_system.ran,
     if deserialize_format == 'json':
         _methods = resp.json()['methods']['method']
     else:
-        _methods = ET.fromstring(resp.data())
+        _methods = xmltodict.parse(resp.data())['ns1:methods']
 
     paymentMethods = {}
     for m in _methods:
-        _method_obj = models.PaymentMethod.deserialize(m, deserialize_format)
-        paymentMethods[m['id']] = _method_obj
+        _method_obj = models.PaymentMethod._dict_to_obj(m)
+        paymentMethods[_method_obj.methodId] = _method_obj
     return paymentMethods
 
 
@@ -62,16 +62,79 @@ def get_one_method():
     return list_paymentMethods().items()[0][1]
 
 
-def get_paymentmethod(methodId):
-    headers = {'accept': 'application/json', 'x-auth-token': get_token()}
+def get_paymentMethod(methodId,
+                      ran=CONF.rax_payment_system.ran,
+                      serialize_format='json',
+                      deserialize_format='json'):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['x-auth-token'] = get_token()
+
     endpoint = urlparse.urljoin(
         CONF.syntribos.endpoint,
-        "/v1/accounts/{0}/methods/{1}".format(CONF.rax_payment_system.ran,
+        "/v1/accounts/{0}/methods/{1}".format(ran,
                                               methodId))
     resp, _ = SynHTTPClient().request(
         "GET", endpoint, headers=headers, sanitize=True)
     m = models.PaymentMethod._dict_to_obj(resp.json())
     return m
+
+
+def create_paymentMethod(methodType,
+                         ran=CONF.rax_payment_system.ran,
+                         serialize_format='json',
+                         deserialize_format='json',
+                         model=None,
+                         **kwargs):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['content-type'] = 'application/%s' % serialize_format
+    headers['x-auth-token'] = get_token()
+
+    endpoint = urlparse.urljoin(
+        CONF.syntribos.endpoint,
+        "/v1/accounts/{0}/methods".format(ran))
+
+    if not model:
+        if 'paymentCard' in methodType:
+            _model_class = models.PaymentCardMethod
+        elif 'electronicCheck' in methodType:
+            _model_class = models.ACHMethod
+        elif 'ukDirectDebit' in methodType:
+            _model_class = models.UKDebitMethod
+        elif 'sepa' in methodType:
+            _model_class = models.SEPAMethod
+        model = _model_class(**kwargs)
+
+    if serialize_format == 'json':
+        data = model._obj_to_dict()
+    else:
+        data = model._obj_to_xml()
+
+    resp, _ = SynHTTPClient().request(
+        "POST", endpoint, data=data, headers=headers, sanitize=True)
+
+    if deserialize_format == 'json':
+        resp_data = resp.json()['papi:method']
+    else:
+        resp_data = xmltodict.parse(resp.data())['ns2:method']
+
+    return type(model)._dict_to_obj(resp_data)
+
+
+def delete_paymentMethod(methodId,
+                         ran=CONF.rax_payment_system.ran,
+                         serialize_format='json',
+                         deserialize_format='json'):
+    headers = {}
+    headers['x-auth-token'] = get_token()
+
+    endpoint = urlparse.urljoin(
+        CONF.syntribos.endpoint,
+        "/v1/accounts/{0}/methods/{1}".format(ran,
+                                              methodId))
+    resp, _ = SynHTTPClient().request(
+        "DELETE", endpoint, headers=headers, sanitize=True)
 
 
 methodValidations = {}
@@ -81,11 +144,18 @@ def create_methodValidation(lineOfBusiness=None,
                             contractEntity=None,
                             currencyCode=None,
                             addressVerificationInformation=None,
-                            method=None):
-    headers = {'accept': 'application/json', 'x-auth-token': get_token(),
-               'content-type': 'application/json'}
+                            method=None,
+                            ran=CONF.rax_payment_system.ran,
+                            serialize_format='json',
+                            deserialize_format='json'):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['content-type'] = 'application/%s' % serialize_format
+    headers['x-auth-token'] = get_token()
+
     endpoint = urlparse.urljoin(CONF.syntribos.endpoint,
                                 "/v1/methodValidations")
+
     if addressVerificationInformation:
         _avi = addressVerificationInformation
     else:
@@ -100,15 +170,27 @@ def create_methodValidation(lineOfBusiness=None,
     method = method or get_one_method()
     if not method.addressVerificationInformation:
         method.addressVerificationInformation = _avi
+
     _mv = models.MethodValidation(
         method=method,
         lineOfBusiness=lineOfBusiness or 'US_CLOUD',
         contractEntity=contractEntity or 'CONTRACT_US',
         currencyCode=currencyCode or 'USD')
-    data = _mv._obj_to_dict()
+
+    if serialize_format == 'json':
+        data = _mv._obj_to_dict()
+    else:
+        data = _mv._obj_to_xml()
+
     resp, _ = SynHTTPClient().request(
         "POST", endpoint, data=data, headers=headers, sanitize=True)
-    validation = models.MethodValidation._dict_to_obj(resp.json())
+
+    if deserialize_format == 'json':
+        _data = resp.json()['papi:methodValidation']
+    else:
+        _data = xmltodict.parse(resp.data())['ns3:methodValidation']
+
+    validation = models.MethodValidation._dict_to_obj(_data)
     methodValidations[validation.methodValidationId] = validation
 
     return validation
@@ -116,26 +198,37 @@ def create_methodValidation(lineOfBusiness=None,
 
 def get_one_methodValidationId():
     if not methodValidations:
-        create_methodValidation()
+        return create_methodValidation().methodValidationId
     return methodValidations.items()[0][0]
 
 
 def get_one_methodValidation():
     if not methodValidations:
-        create_methodValidation()
+        return create_methodValidation()
     return methodValidations.items()[0][1]
 
 
 @memoize
-def list_payments():
-    headers = {'accept': 'application/json', 'x-auth-token': get_token(),
-               'content-type': 'application/json'}
+def list_payments(ran=CONF.rax_payment_system.ran,
+                  serialize_format='json',
+                  deserialize_format='json'):
+
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['x-auth-token'] = get_token()
+
     endpoint = urlparse.urljoin(
         CONF.syntribos.endpoint,
-        "/v1/accounts/%s/payments" % CONF.rax_payment_system.ran)
+        "/v1/accounts/%s/payments" % ran)
+
     resp, _ = SynHTTPClient().request(
         "GET", endpoint, headers=headers, sanitize=True)
-    _payments = resp.json()['payments']['payment']
+
+    if deserialize_format == 'json':
+        _payments = resp.json()['payments']['payment']
+    else:
+        _payments = xmltodict.parse(resp.data())['ns1:payments']
+
     payments = {}
     for p in _payments:
         payments[p['id']] = models.Payment._dict_to_obj(p)
@@ -150,16 +243,59 @@ def get_one_payment():
     return list_payments().items()[0][1]
 
 
+def create_payment(ran=CONF.rax_payment_system.ran,
+                   serialize_format='json',
+                   deserialize_format='json',
+                   model=None,
+                   **kwargs):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['content-type'] = 'application/%s' % serialize_format
+    headers['x-auth-token'] = get_token()
+
+    endpoint = urlparse.urljoin(
+        CONF.syntribos.endpoint,
+        "/v1/accounts/{0}/payments".format(ran))
+
+    if not model:
+        model = models.Payment(**kwargs)
+
+    if serialize_format == 'json':
+        data = model._obj_to_dict()
+    else:
+        data = model._obj_to_xml()
+
+    resp, _ = SynHTTPClient().request(
+        "POST", endpoint, data=data, headers=headers, sanitize=True)
+
+    if deserialize_format == 'json':
+        resp_data = resp.json()['papi:payment']
+    else:
+        resp_data = xmltodict.parse(resp.data())['ns2:payment']
+
+    return models.Payment._dict_to_obj(resp_data)
+
+
 @memoize
-def list_voids():
-    headers = {'accept': 'application/json', 'x-auth-token': get_token(),
-               'content-type': 'application/json'}
+def list_voids(ran=CONF.rax_payment_system.ran,
+               serialize_format='json',
+               deserialize_format='json'):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['x-auth-token'] = get_token()
+
     endpoint = urlparse.urljoin(
         CONF.syntribos.endpoint,
         "/v1/payments/%s/voids" % get_one_paymentId())
+
     resp, _ = SynHTTPClient().request(
         "GET", endpoint, headers=headers, sanitize=True)
-    _voids = resp.json()['voids']['void']
+
+    if deserialize_format == 'json':
+        _voids = resp.json()['voids']['void']
+    else:
+        _voids = xmltodict.parse(resp.data())['ns1:voids']
+
     voids = {}
     for v in _voids:
         voids[v['id']] = models.Void._dict_to_obj(v)
@@ -174,16 +310,58 @@ def get_one_void():
     return list_voids().items()[0][1]
 
 
-@memoize
-def list_refunds():
-    headers = {'accept': 'application/json', 'x-auth-token': get_token(),
-               'content-type': 'application/json'}
+def create_void(paymentId,
+                serialize_format='json',
+                deserialize_format='json',
+                model=None,
+                **kwargs):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['content-type'] = 'application/%s' % serialize_format
+    headers['x-auth-token'] = get_token()
+
     endpoint = urlparse.urljoin(
         CONF.syntribos.endpoint,
-        "/v1/accounts/%s/refunds" % CONF.rax_payment_system.ran)
+        "/v1/payments/{0}/voids".format(paymentId))
+
+    if not model:
+        model = models.Void(**kwargs)
+
+    if serialize_format == 'json':
+        data = model._obj_to_dict()
+    else:
+        data = model._obj_to_xml()
+
+    resp, _ = SynHTTPClient().request(
+        "POST", endpoint, data=data, headers=headers, sanitize=True)
+
+    if deserialize_format == 'json':
+        resp_data = resp.json()['papi:void']
+    else:
+        resp_data = xmltodict.parse(resp.data())['ns3:void']
+
+    return models.Void._dict_to_obj(resp_data)
+
+
+@memoize
+def list_refunds(ran=CONF.rax_payment_system.ran,
+                 serialize_format='json',
+                 deserialize_format='json'):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['x-auth-token'] = get_token()
+
+    endpoint = urlparse.urljoin(
+        CONF.syntribos.endpoint,
+        "/v1/accounts/%s/refunds" % ran)
+
     resp, _ = SynHTTPClient().request(
         "GET", endpoint, headers=headers, sanitize=True)
-    _refunds = resp.json()['refunds']['refund']
+
+    if deserialize_format == 'json':
+        _refunds = resp.json()['refunds']['refund']
+    else:
+        _refunds = xmltodict.parse(resp.data())['ns1:refunds']
     refunds = {}
     for r in _refunds:
         refunds[r['id']] = models.Refund._dict_to_obj(r)
@@ -196,3 +374,36 @@ def get_one_refundId():
 
 def get_one_refund():
     return list_refunds().items()[0][1]
+
+
+def create_refund(ran,
+                  serialize_format='json',
+                  deserialize_format='json',
+                  model=None,
+                  **kwargs):
+    headers = {}
+    headers['accept'] = 'application/%s' % deserialize_format
+    headers['content-type'] = 'application/%s' % serialize_format
+    headers['x-auth-token'] = get_token()
+
+    endpoint = urlparse.urljoin(
+        CONF.syntribos.endpoint,
+        "/v1/accounts/{0}/refunds".format(ran))
+
+    if not model:
+        model = models.Void(**kwargs)
+
+    if serialize_format == 'json':
+        data = model._obj_to_dict()
+    else:
+        data = model._obj_to_xml()
+
+    resp, _ = SynHTTPClient().request(
+        "POST", endpoint, data=data, headers=headers, sanitize=True)
+
+    if deserialize_format == 'json':
+        resp_data = resp.json()['papi:refund']
+    else:
+        resp_data = xmltodict.parse(resp.data())['ns2:refund']
+
+    return models.Refund._dict_to_obj(resp_data)
